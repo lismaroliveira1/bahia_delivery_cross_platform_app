f8034edc16bcd6ff74641145ac6460da07704760
lismaroliveira1 <englismaroliveira@gmail.com>
2020-08-12 12:13:21 -0300

    AppBar Created

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:bahia_delivery/themes/theme.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -13,14 +14,49 @@ class HomeTab extends StatelessWidget {
         CustomScrollView(
           slivers: <Widget>[
             SliverAppBar(
+              leading: GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(13)),
+                      color: Colors.white,
+                      boxShadow: AppTheme.shadow),
+                  child: Icon(Icons.sort),
+                ),
+                onTap: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
               floating: true,
               snap: true,
               backgroundColor: Colors.transparent,
               elevation: 0.0,
-              title: TextField(
-                decoration: InputDecoration(
-                    icon: Icon(Icons.search), hintText: "Vai de que hoje?"),
+              title: Container(
+                padding: EdgeInsets.all(10),
+                height: 80,
+                width: 80,
+                child: Image(
+                  image: AssetImage("images/logo.png"),
+                ),
               ),
+              centerTitle: true,
+              actions: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: GestureDetector(
+                      child: Container(
+                        height: 50,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          image: DecorationImage(
+                              image: AssetImage("images/user.png"),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      onTap: () {}),
+                ),
+              ],
             ),
           ],
         )
