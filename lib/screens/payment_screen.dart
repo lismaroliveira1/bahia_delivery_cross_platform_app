b6c8e78d97de4c6e917b5dc94d27b4510f322511
lismaroliveira1 <englismaroliveira@gmail.com>
2020-09-22 20:34:55 -0300

    IOs Settings

---
+++ lib/screens/payment_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PaymentScreen extends StatefulWidget {
+  @override
+  _PaymentScreenState createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Formas de pagamentos"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
