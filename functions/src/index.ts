e7fae5c601339a6a8c7f9def985d28a60654b4ea
lismaroliveira1 <englismaroliveira@gmail.com>
2020-09-26 18:09:33 -0300

    cielo configurated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin'
-
+import { CieloConstructor, Cielo, TransactionCreditCardRequestModel, CaptureRequestModel, CancelTransactionRequestModel, EnumBrands } from 'cielo';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+const merchantId = functions.config().cielo.merchantid;
+const merchantkey = functions.config().cielo.merchantkey;
+const cieloParams: CieloConstructor = {
+    merchantId: merchantId,
+    merchantKey: merchantkey,
+    sandbox: true,
+    debug: true,
+}
+
+const cielo = new Cielo(cieloParams);
 export const helloWorld = functions.https.onCall((data, context) => {
     return { 
         data: "Hello from cloud functions!!!!"
