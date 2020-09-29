55011e229a829616f5ff11253f30803e69de2631
lismaroliveira1 <englismaroliveira@gmail.com>
2020-09-29 15:41:53 -0300

    Store Home Tab

--- lib/screens/store_home_screen.dart
+++ lib/screens/store_home_screen.dart
@@ -1,8 +1,11 @@
+import 'package:bahia_delivery/tabs/store_home_tab.dart';
 import 'package:flutter/material.dart';
 
 class StoreHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: StoreHomeTab(),
+    );
   }
 }
