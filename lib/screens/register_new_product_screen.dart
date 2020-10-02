8624bca45ab3b29d868f4c88632060435e7a5e68
lismaroliveira1 <englismaroliveira@gmail.com>
2020-10-02 17:30:02 -0300

    register new product tab

--- lib/screens/register_new_product_screen.dart
+++ lib/screens/register_new_product_screen.dart
@@ -1,12 +1,11 @@
+import 'package:bahia_delivery/tabs/register_new_product_tab.dart';
 import 'package:flutter/material.dart';
 
 class RegisterNewProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-      ),
+      body: RegisterNewProductTab(),
     );
   }
 }
---
+++ lib/tabs/register_new_product_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterNewProductTab extends StatefulWidget {
+  @override
+  _RegisterNewProductTabState createState() => _RegisterNewProductTabState();
+}
+
+class _RegisterNewProductTabState extends State<RegisterNewProductTab> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[],
+    );
+  }
+}
