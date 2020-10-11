4efb6b6fb3dce53c79ef73d685c947cd8ce03cc8
lismaroliveira1 <englismaroliveira@gmail.com>
2020-10-11 13:48:58 -0300

    oder data

--- lib/data/order_data.dart
+++ lib/data/order_data.dart
@@ -1,1 +1,25 @@
-class OrderData {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class OrderData {
+  String storeId;
+  String date;
+  String price;
+  String shipPrice;
+  String storeImage;
+  String storeDescription;
+  String deliveryManName;
+
+  OrderData({
+    @required this.storeId,
+    @required this.storeImage,
+    @required this.storeImage,
+    @required this.storeDescription,
+    @required this.date,
+    @required this.deliveryManName,
+    @required this.price,
+    @required this.shipPrice,
+  });
+
+  OrderData.fromDocument(DocumentSnapshot documentSnapshot) {}
+}
