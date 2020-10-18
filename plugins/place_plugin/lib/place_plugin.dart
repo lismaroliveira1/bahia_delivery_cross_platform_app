9df32339a6acc0a80feca4d322defa8ec5ba5338
lismaroliveira1 <englismaroliveira@gmail.com>
2020-10-18 09:19:36 -0300

    place plugin

--- plugins/place_plugin/lib/place_plugin.dart
+++ plugins/place_plugin/lib/place_plugin.dart
@@ -1,14 +1,35 @@
-
-import 'dart:async';
-
 import 'package:flutter/services.dart';
+import 'package:place_plugin/place.dart';
 
 class PlacePlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('place_plugin');
+  static const MethodChannel _channel = const MethodChannel('place_plugin');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static void intialize(String apiKey) async {
+    await _channel.invokeMethod("initialize", <String, dynamic>{
+      'apikey': apiKey,
+    });
+  }
+
+  static Future<List<Place>> search(String keyword) async {
+    var result = await _channel.invokeMethod("search", <String, dynamic>{
+      'keyword': keyword,
+    });
+    if (result != null) {
+      return Place.fromNative(result);
+    }
+    return [];
+  }
+
+  static Future<Place> getPlace(Place place) async {
+    var result = await _channel.invokeMapMethod('getPlace', <String, dynamic>{
+      'placeId': place.placeId,
+    });
+    if (result != null) {
+      place.lat = double.parse(result["latitude"].toString());
+      place.lng = double.parse(result["longitude"].toString());
+      place.formatedAddress = result["formatAddress"];
+      return place;
+    }
+    return null;
   }
 }
