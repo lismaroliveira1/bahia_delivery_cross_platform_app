2316abc7d206932b7d03cecead6ff19c9e651609
lismaroliveira1 <englismaroliveira@gmail.com>
2020-12-15 19:34:09 -0300

    birth day calendar configured

--- lib/tabs/register_partner_with_cpf_tab.dart
+++ lib/tabs/register_partner_with_cpf_tab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class RegisterPartnerWithCPFTab extends StatefulWidget {
@@ -10,93 +9,262 @@ class RegisterPartnerWithCPFTab extends StatefulWidget {
 
 class _RegisterPartnerWithCPFTabState extends State<RegisterPartnerWithCPFTab> {
   TextEditingController _nameController = TextEditingController();
-  String _selectedDate;
-  String _dateCount;
-  String _range;
-  String _rangeCount;
+  TextEditingController _cpfController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  DateTime _selectedDate;
+  String _textDate;
+
   @override
   void initState() {
-    _selectedDate = '';
-    _dateCount = '';
-    _range = '';
-    _rangeCount = '';
+    _textDate = "";
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     double _imageSize = MediaQuery.of(context).size.width / 3;
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Center(
-                child: Container(
-                  height: _imageSize,
-                  width: _imageSize,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    image: DecorationImage(
-                      image: AssetImage(
-                        'images/logo_and_name.jpg',
+    return Form(
+      key: _formKey,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    height: _imageSize,
+                    width: _imageSize,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'images/logo_and_name.jpg',
+                        ),
+                        fit: BoxFit.cover,
                       ),
-                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25.0),
-                child: Text(
-                  'Deixe-nos saber \n mais sobre você',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 26,
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 18),
-                child: TextField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    hintText: "Fulano de tal",
-                    labelText: 'Nome Completo',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
+                Padding(
+                  padding: const EdgeInsets.only(top: 25.0),
+                  child: Text(
+                    'Deixe-nos saber \n mais sobre você',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 26,
                     ),
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                  Container(
-                      child: FlatButton(
+                Container(
+                  margin: EdgeInsets.only(top: 18),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      hintText: "Fulano de tal",
+                      labelText: 'Nome Completo',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 18),
+                  child: TextField(
+                    controller: _cpfController,
+                    decoration: InputDecoration(
+                      hintText: '000.000.000-00',
+                      labelText: 'CPF',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: FlatButton(
                     onPressed: () {
                       return showDialog(
                         context: context,
-                        builder: (_) => Container(
-                          height: MediaQuery.of(context).size.height / 4,
-                          width: MediaQuery.of(context).size.width / 4,
-                          color: Colors.white,
-                        ),
+                        builder: (BuildContext context) {
+                          return Dialog(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: Container(
+                              height: MediaQuery.of(context).size.height / 2,
+                              width: MediaQuery.of(context).size.width / 1.2,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(
+                                      "Selecione sua data \nde nascimento",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: SfDateRangePicker(
+                                        onSelectionChanged: _onSelectionChanged,
+                                        selectionMode:
+                                            DateRangePickerSelectionMode.single,
+                                        initialSelectedRange: PickerDateRange(
+                                            DateTime.now().subtract(
+                                                const Duration(days: 4)),
+                                            DateTime.now()
+                                                .add(const Duration(days: 3))),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 10,
+                                      vertical: 20,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Center(
+                                          child: FlatButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            padding: EdgeInsets.zero,
+                                            child: Container(
+                                              height: 40,
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width /
+                                                  3.5,
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(12),
+                                                  color: Colors.red),
+                                              child: Center(
+                                                child: Text(
+                                                  "Cancelar",
+                                                  textAlign: TextAlign.center,
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.w600,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
                       );
                     },
-                    child: Text(
-                      '__/__/_____',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 28,
-                        color: Colors.black38,
-                      ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 28.0),
+                          child: Icon(
+                            Icons.perm_contact_calendar,
+                            size: MediaQuery.of(context).size.width / 3.5,
+                            color: Colors.black45,
+                          ),
+                        ),
+                        Text(
+                          "Nascimento\n$_textDate",
+                        ),
+                      ],
                     ),
-                  )),
-                ],
-              ),
-            ],
+                  ),
+                ),
+                Container(
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 28.0),
+                          child: Icon(
+                            Icons.location_on_rounded,
+                            size: MediaQuery.of(context).size.width / 3.5,
+                            color: Colors.black45,
+                          ),
+                        ),
+                        Text(
+                          "Localização",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 26),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FlatButton(
+                        onPressed: () {},
+                        padding: EdgeInsets.zero,
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 3.5,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.red),
+                          child: Center(
+                            child: Text(
+                              "Cancelar",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      FlatButton(
+                        onPressed: () {},
+                        padding: EdgeInsets.zero,
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 3.5,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.red),
+                          child: Center(
+                            child: Text(
+                              "Próximo",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -105,20 +273,13 @@ class _RegisterPartnerWithCPFTabState extends State<RegisterPartnerWithCPFTab> {
 
   void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
     setState(() {
-      if (args.value is PickerDateRange) {
-        _range =
-            DateFormat('dd/MM/yyyy').format(args.value.startDate).toString() +
-                ' - ' +
-                DateFormat('dd/MM/yyyy')
-                    .format(args.value.endDate ?? args.value.startDate)
-                    .toString();
-      } else if (args.value is DateTime) {
-        _selectedDate = args.value;
-      } else if (args.value is List<DateTime>) {
-        _dateCount = args.value.length.toString();
-      } else {
-        _rangeCount = args.value.length.toString();
-      }
+      _selectedDate = args.value;
+      _textDate = _selectedDate.day.toString() +
+          '/' +
+          _selectedDate.month.toString() +
+          '/' +
+          _selectedDate.year.toString();
     });
+    Navigator.of(context).pop();
   }
 }
