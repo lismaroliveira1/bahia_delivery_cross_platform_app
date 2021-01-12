6ab9a26129277c988752ef4610d37daa30fec504
lismaroliveira1 <englismaroliveira@gmail.com>
2021-01-12 09:19:52 -0300

    cart list tab

---
+++ lib/tabs/cart_list_tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CartListTab extends StatefulWidget {
+  @override
+  _CartListTabState createState() => _CartListTabState();
+}
+
+class _CartListTabState extends State<CartListTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
